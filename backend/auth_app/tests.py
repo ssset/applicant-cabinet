648f93ffdb98dec95661d79a82c5f5053228e44e
ssset <claimov@gmail.com>
2025-03-12 23:22:13 +0300

    test: add basic registration test

--- backend/auth_app/tests.py
+++ backend/auth_app/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from users.models import CustomUser
 
-# Create your tests here.
+
+class AuthTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_register_success(self):
+        """
+        Тест успешной регистрации
+        """
+        data = {
+            'email': 'test@example.com',
+            'password': 'password123',
+            'password2': 'password123',
+            'role': 'applicant'
+        }
+        response = self.client.post('/api/auth/register/', data, format='json')
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(CustomUser.objects.filter(email='test@example.com').exists())
